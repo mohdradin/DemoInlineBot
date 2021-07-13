c84539ba029b82befe311ad95bdbb2bcc5984a87
Bots.Business <git.sync@bots.business>
2021-07-13 10:22:58 +0000

    Bots.Business export for bot id: 371611

---
+++ commands/_start.js
@@ -0,0 +1,15 @@
+/*CMD
+  command: /start
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+It is demo for inline keyboard.
+Try now inline /keyboard
+  ANSWER
+  keyboard: 
+  aliases: 
+CMD*/
+
